636e3e601cd16f2f954c388da046d737420901d2
Михаил Насонов <mnasonovy@gmail.com>
2023-10-24 18:47:34 +0400

    implementation of a doubly connected linear connected list

--- Lab2/linked_list.cc
+++ Lab2/linked_list.cc
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <random>
+#include <stdexcept>
 
 template<typename T>
 struct Node {
@@ -11,28 +14,191 @@ struct Node {
 template<typename T>
 class LinkedList {
 private:
-    Node<T>* head;
-    Node<T>* tail; 
+    Node<T>* head_;
+    Node<T>* tail_; 
 
 public:
-    LinkedList();
-    LinkedList(const LinkedList<T>& other);
-    LinkedList(std::size_t count, int minValue, int maxValue);
-    ~LinkedList();
-    LinkedList<T>& operator=(const LinkedList<T>& other);
-    void push_tail(const T& value);
-    void push_tail(const LinkedList<T>& other);
-    void push_head(const T& value);
-    void push_head(const LinkedList<T>& other);
-    void pop_head();
-    void pop_tail();
-    void delete_node(const T& value);
-    const T& operator[](std::size_t index);
-    T& operator[](std::size_t index);
-    void clear();
+    LinkedList() : head_(nullptr), tail_(nullptr) {}
+
+    LinkedList(const LinkedList<T>& other) : head_(nullptr), tail_(nullptr) {
+        copyFrom(other);
+    }
+
+    LinkedList(std::size_t count, int minValue, int maxValue) : head_(nullptr), tail_(nullptr) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> dist(minValue, maxValue);
+
+        for (std::size_t i = 0; i < count; ++i) {
+            T value = dist(gen);
+            push_tail(value);
+        }
+    }
+
+    ~LinkedList() {
+        clear();
+    }
+
+    LinkedList<T>& operator=(const LinkedList<T>& other) {
+        if (this != &other) {
+            clear();
+            copyFrom(other);
+        }
+        return *this;
+    }
+
+    void push_tail(const T& value) {
+        Node<T>* tmp = new Node<T>(value);
+        if (tail == nullptr) {
+            head_ = tmp;
+            tail_ = tmp;
+        }
+        else {
+            tail->next = tmp;
+            tmp->prev = tail_;
+            tail_ = tmp;
+        }
+    }
+    void push_tail(const LinkedList<T>& other) {
+        Node<T>* tmp = other.head_;
+        while (tmp != nullptr) {
+            push_tail(tmp->data);
+            tmp = tmp->next;
+        }
+    }
+
+    void push_head(const T& value) {
+        Node<T>* tmp = Node<T>(value);
+        if (head_ == nullptr) {
+            head_ = tmp;
+            tail_ = tmp;
+        }
+        else {
+            tmp->next = head_;
+            head_->prev = tmp;
+            head_ = tmp;
+        }
+    }
+    void push_head(const LinkedList<T>& other) {
+        Node<T>* tmp = other.tail;
+        while (tmp != nullptr) {
+            push_head(tmp->data);
+            tmp = tmp->prev;
+        }
+    }
+
+    void pop_head() {
+        if (head == nullptr) {
+            throw std::runtime_error("List is empty.");
+        }
+
+        Node<T>* tmp = head_;
+        head_ = head_->next;
+        if (head_ != nullptr) {
+            head_->prev = nullptr;
+        }
+        else {
+            tail_ = nullptr;
+        }
+        delete tmp;
+    }
+
+    void pop_tail() {
+        if (tail_ == nullptr) {
+            throw std::runtime_error("List is empty.");
+        }
+
+        Node<T>* tmp = tail_;
+        tail_ = tail_->prev;
+        if (tail_ != nullptr) {
+            tail_->next = nullptr;
+        }
+        else {
+            head_ = nullptr;
+        }
+        delete tmp;
+    }
+
+    void delete_node(const T& value) {
+        Node<T>* tmp = head_;
+        while (tmp != nullptr) {
+            if (tmp->data == value) {
+                if (tmp == head_) {
+                    head_ = head_->next;
+                    if (head_ != nullptr) {
+                        head_->prev = nullptr;
+                    }
+                }
+                else if (tmp == tail_) {
+                    tail_ = tail_->prev;
+                    if (tail_ != nullptr) {
+                        tail_->next = nullptr;
+                    }
+                }
+                else {
+                    tmp->prev->next = tmp->next;
+                    tmp->next->prev = tmp->prev;
+                }
+
+                Node<T>* toDelete = tmp;
+                tmp = tmp->next;
+                delete toDelete;
+            }
+            else {
+                tmp = tmp->next;
+            }
+        }
+    }
+
+    const T& operator[](std::size_t index) const {
+        Node<T>* current = head_;
+        std::size_t i = 0;
+        while (current != nullptr && i < index) {
+            current = current->next;
+            ++i;
+        }
+
+        if (current == nullptr) {
+            throw std::out_of_range("Index out of range.");
+        }
+
+        return current->data;
+    }
+
+    T& operator[](std::size_t index) {
+        Node<T>* current = head_;
+        std::size_t i = 0;
+        while (current != nullptr && i < index) {
+            current = current->next;
+            ++i;
+        }
+
+        if (current == nullptr) {
+            throw std::out_of_range("Index out of range.");
+        }
+
+        return current->data;
+    }
+
+    void clear() {
+        Node<T>* current = head_;
+        while (current != nullptr) {
+            Node<T>* toDelete = current;
+            current = current->next;
+            delete toDelete;
+        }
+        head_ = nullptr;
+        tail_ = nullptr;
+    }
 
 private:
     // Помощник для копирования списка
-    void copyFrom(const LinkedList<T>& other);
+    void copyFrom(const LinkedList<T>& other) {
+        Node<T>* otherNode = other.head;
+        while (otherNode != nullptr) {
+            push_tail(otherNode->data);
+            otherNode = otherNode->next;
+        }
+    }
 };
 
